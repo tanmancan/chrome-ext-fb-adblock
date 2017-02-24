b086c301cf1ba3f034178b21bd28fce4ca235f92
tanmancan <contact@tkarimdesign.com>
2017-02-24 09:18:52 -0500

    cleaned up code. wrapper removal in raf

--- fb-adblock.js
+++ fb-adblock.js
@@ -1,39 +1,36 @@
-var showConsole = true;
+const showConsoleLog = true;
+const selectors = '#facebook a[rel="nofollow"]';
 
-window.addEventListener('load', function(e) {
-  var sponsoredLink = document.querySelectorAll('a[rel="nofollow"');
+window.addEventListener('load', checkSponsoredLink, false);
+document.addEventListener('scroll', checkSponsoredLink, false);
+
+function checkSponsoredLink(e) {
+  let sponsoredLink = document.querySelectorAll(selectors);
   if(sponsoredLink) {
     sponsoredLink.forEach(function(el,idx) {
       el.style.background = 'red';
       recurseFindParent(el);
     });
   }
-}, false);
-
-document.addEventListener('scroll', function(e) {
-  var sponsoredLink = document.querySelectorAll('a[rel="nofollow"');
-  if(sponsoredLink) {
-    sponsoredLink.forEach(function(el,idx) {
-      el.style.background = 'red';
-      recurseFindParent(el);
-    });
-  }
-}, false);
+}
 
 
 function recurseFindParent(child) {
-  var parent = null,
-      targetCheck = null;
+  let parent = null,
+      targetCheck = null,
+      raf = window.requestAnimationFrame;
   while(!parent) {
     targetCheck = child.parentElement.getAttribute('data-fte');
     if(!targetCheck) {
       child = child.parentElement;
+
       clog('==================================');
       clog('PARENT-NOT-FOUND');
       clog('child:', child);
       clog('parent:', child.parentElement);
       clog('parent.attr:', child.parentElement.getAttribute('data-fte'));
       clog('==================================');
+
     }else {
       clog('==================================');
       clog('PARENT-FOUND');
@@ -41,15 +38,20 @@ function recurseFindParent(child) {
       clog('parent:', child.parentElement);
       clog('parent.attr:', child.parentElement.getAttribute('data-fte'));
       clog('==================================');
+
       parent = child.parentElement;
-      console.log('Removing', parent);
-      parent.remove();
+
+      clog('Removing', parent);
+
+      raf(function() {
+        parent.remove();
+      });
     }
   }
 }
 
 function clog(msg, el) {
-  if(showConsole) {
+  if(showConsoleLog) {
     console.log(msg, el);
   }
 }
